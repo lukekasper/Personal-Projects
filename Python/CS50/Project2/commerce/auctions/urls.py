d3c697ec10f50c87d79d53007c4e38b7adf3ade2
lukekasper <lukekasper25@gmail.com>
2022-10-10 16:12:10 -0400

    Add files via upload

---
+++ Python/CS50/Project2/commerce/auctions/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("new listing", views.new_listing, name="new_listing"),
+    path("register", views.register, name="register")
+]
