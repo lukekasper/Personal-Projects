1e48ab0107847c4101c309cc43f9bbdf2409b02d
lukekasper <lukekasper25@gmail.com>
2023-07-24 14:38:48 -0400

    Update settings.py

--- CS50/Recipes/Recipes/settings.py
+++ CS50/Recipes/Recipes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,14 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&l9i%^s3^l(1l*u+op7)hl!*t$u^q%@y_b7*!l8lma#-a$bxoo'
+# SECURITY WARNING: keep the secret key used in production secret! (contained in the secret.env file)
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -74,14 +74,34 @@ WSGI_APPLICATION = 'Recipes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# Get the admin username and password from environment variables, or use default values
+ADMIN_USER = os.environ.get('MYSQL_USER', 'default_admin')
+ADMIN_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'default_password')
+
+ADMINS = [
+    (ADMIN_USER, ADMIN_PASSWORD),
+]
+
+# Set the default superuser credentials if they are not provided via environment variables
+if not os.environ.get('DJANGO_SUPERUSER_USERNAME'):
+    os.environ['DJANGO_SUPERUSER_USERNAME'] = ADMIN_USER
+if not os.environ.get('DJANGO_SUPERUSER_PASSWORD'):
+    os.environ['DJANGO_SUPERUSER_PASSWORD'] = ADMIN_PASSWORD
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'recipies_db',
+        'USER': ADMIN_USER,
+        'PASSWORD': ADMIN_PASSWORD,
+        'HOST': 'mysql',  # Use the service name of the MySQL container
+        'PORT': '3306',  # Use the default MySQL port unless you've specified a different one
     }
 }
 
 AUTH_USER_MODEL = "cookbook.User"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
