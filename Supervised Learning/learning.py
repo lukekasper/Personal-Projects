fffabf2efbf617ece1074171af7d11c1537e275a
lukekasper <lukekasper25@gmail.com>
2024-02-29 12:23:33 -0500

    Update learning.py

--- Supervised Learning/learning.py
+++ Supervised Learning/learning.py
@@ -111,3 +111,33 @@ plt.ylabel("Sales ($)")
 
 # Display the plot
 plt.show()
+
+## Train/Test and Model Accuracy ##
+# Create X and y arrays
+X = sales_df.drop("sales", axis=1).values
+y = sales_df["sales"].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Instantiate the model
+reg = LinearRegression()
+
+# Fit the model to the data
+reg.fit(X_train, y_train)
+
+# Make predictions
+y_pred = reg.predict(X_test)
+print("Predictions: {}, Actual Values: {}".format(y_pred[:2], y_test[:2]))
+
+# Import mean_squared_error
+from sklearn.metrics import mean_squared_error
+
+# Compute R-squared
+r_squared = reg.score(X_test, y_test)
+
+# Compute RMSE
+rmse = mean_squared_error(y_test, y_pred, squared=False)
+
+# Print the metrics
+print("R^2: {}".format(r_squared))
+print("RMSE: {}".format(rmse))
