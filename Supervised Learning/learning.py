b97272b628accc048e43fe8f6ffd36464a3d2b07
lukekasper <lukekasper25@gmail.com>
2024-02-29 17:49:00 -0500

    Update learning.py

--- Supervised Learning/learning.py
+++ Supervised Learning/learning.py
@@ -419,3 +419,48 @@ print(cv.best_score_, "\n", cv.best_params_)
 
 ## Evaluating Multiple Models ##
 # Best to scale data before evaluating models out of the box; typically no tuning for evaluation
+
+# Loop through the models' values
+for model in models.values():
+  kf = KFold(n_splits=6, random_state=42, shuffle=True)
+  
+  # Perform cross-validation
+  cv_scores = cross_val_score(model, X_train, y_train, cv=kf)
+  
+  # Append the results
+  results.append(cv_scores)
+
+# Create a box plot of the results
+plt.boxplot(results, labels=models.keys())
+plt.show()
+
+# Import mean_squared_error
+from sklearn.metrics import mean_squared_error
+
+for name, model in models.items():
+  
+  # Fit the model to the training data
+  model.fit(X_train_scaled, y_train)
+  
+  # Make predictions on the test set
+  y_pred = model.predict(X_test_scaled)
+  
+  # Calculate the test_rmse
+  test_rmse = mean_squared_error(y_test, y_pred, squared=False)
+  print("{} Test Set RMSE: {}".format(name, test_rmse))
+
+# Create models dictionary
+models = {"Logistic Regression": LogisticRegression(), "KNN": KNeighborsClassifier(), "Decision Tree Classifier": DecisionTreeClassifier()}
+results = []
+
+# Loop through the models' values
+for model in models.values():
+  
+  # Instantiate a KFold object
+  kf = KFold(n_splits=6, random_state=12, shuffle=True)
+  
+  # Perform cross-validation
+  cv_results = cross_val_score(model, X_train_scaled, y_train, cv=kf)
+  results.append(cv_results)
+plt.boxplot(results, labels=models.keys())
+plt.show()
