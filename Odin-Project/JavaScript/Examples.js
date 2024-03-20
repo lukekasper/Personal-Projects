116d1c57f47a62c9c036e98fcb9058fe17d64556
lukekasper <lukekasper25@gmail.com>
2024-03-20 12:56:39 -0400

    Create Examples.js

---
+++ Odin-Project/JavaScript/Examples.js
@@ -0,0 +1,23 @@
+// Javascript Objects
+const myObject = {
+  property: 'Value!',
+  otherProperty: 77,
+  "obnoxious property": function() {
+    // do stuff!
+  }
+};
+
+// dot notation
+myObject.property; // 'Value!'
+
+// Object constructor
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+  this.sayName = function() {
+    console.log(this.name)
+  };
+}
+
+const player = new Player('steve', 'X');
+player1.sayName(); // logs 'steve'
