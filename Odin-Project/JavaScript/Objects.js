1437ba4d9502eff0a23fa5c90baff499e6164532
lukekasper <lukekasper25@gmail.com>
2024-03-20 16:29:48 -0400

    Update Objects.js

--- Odin-Project/JavaScript/Objects.js
+++ Odin-Project/JavaScript/Objects.js
@@ -146,21 +146,3 @@ class Point {
 }
 const myPoint = new Point(95, 165);
 myPoint.log();
-
-
-// Practice:
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read
-  this.info = function() {
-    const read_str = this.read ? "read" : "not read yet";
-    const info_str = this.title + "by" + this.author + ", " + this.pages.toString() + " pages, " + read_str;
-    return info_str
-}
-const donQuixote = new Book("Don Quixote", "Miguel de Cervantes", 909, True);
-console.log(donQuixote.info());
-
-
-//////////////////////////////////////// Library Project ////////////////////////////////////////
