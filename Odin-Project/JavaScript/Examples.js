30719ea592f5d261931a44272924a5f00060314d
lukekasper <lukekasper25@gmail.com>
2024-03-20 13:07:12 -0400

    Update Examples.js

--- Odin-Project/JavaScript/Examples.js
+++ Odin-Project/JavaScript/Examples.js
@@ -21,3 +21,17 @@ function Player(name, marker) {
 
 const player = new Player('steve', 'X');
 player1.sayName(); // logs 'steve'
+
+// Practice:
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read
+  this.info = function() {
+    const read_str = this.read ? "read" : "not read yet";
+    const info_str = this.title + "by" + this.author + ", " + this.pages.toString() + " pages, " + read_str;
+    return info_str
+}
+const donQuixote = new Book("Don Quixote", "Miguel de Cervantes", 909, True);
+console.log(donQuixote.info();)
