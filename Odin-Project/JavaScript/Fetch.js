1f4d132ad16e5c4ea7af6851ad52b914eeb6d7f6
lukekasper <lukekasper25@gmail.com>
2024-04-03 09:47:45 -0400

    Delete Odin-Project/JavaScript/Fetch.js

--- Odin-Project/JavaScript/Fetch.js
+++
@@ -1,7 +0,0 @@
-// fetch with await
-async function logMovies() {
-  const response = await fetch("http://example.com/movies.json");
-  const movies = await response.json();
-  console.log(movies);
-}
-
