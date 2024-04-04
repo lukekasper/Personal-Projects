ac7576d96113d90d523504144189a0eab79adfba
lukekasper <lukekasper25@gmail.com>
2024-04-04 10:40:25 -0400

    Update MockAPI_Test.js

--- Odin-Project/JavaScript/MockAPI_Test.js
+++ Odin-Project/JavaScript/MockAPI_Test.js
@@ -1,41 +1,50 @@
 // const orderTotal = require('./order-total')
 
-const emptyFunction = () => {}
+const orderTotal = require('./order-total')
 
 it('calls vatapi.com correctly', () => {
-  let isFakeFetchCalled = false
-  const fakeFetch = (url) => {
-    expect(url).toBe('https://vatapi.com/v1/country-code-check?code=DE')
-    isFakeFetchCalled = true
+  const fakeProcess = {
+    env: {
+      VAT_API_KEY: 'key123'
+    }
   }
-  orderTotal(fakeFetch, {
+  const fakeFetch = (url, opts) => {
+    expect(opts.headers.apikey).toBe('key123')
+    expect(url).toBe('https://vatapi.com/v1/country-code-check?code=DE')
+    return Promise.resolve({
+      json: () => Promise.resolve({
+        rates: {
+          standard: {
+            value: 19
+          }
+        }
+      })
+    })
+  }
+  return orderTotal(fakeFetch, fakeProcess, {
     country: 'DE',
     items: [
       { 'name': 'Dragon waffles', price: 20, quantity: 2 }
     ]
-  }).then(result => {
-    expect(isFakeFetchCalled).toBe(true)
-  })
+  }).then(result => expect(result).toBe(20*2*1.19))
 })
 
-it('if country code specified')
-
 it('Quantity', () =>
-  orderTotal(emptyFunction, {
+  orderTotal(null, null, {
     items: [
       { 'name': 'Dragon candy', price: 2, quantity: 3 }
     ]
   }).then(result => expect(result).toBe(6)))
 
 it('No quantity specified', () =>
-  orderTotal(emptyFunction, {
+  orderTotal(null, null, {
     items: [
       { 'name': 'Dragon candy', price: 3 }
     ]
   }).then(result => expect(result).toBe(3)))
 
 it('Happy path (Example 1)', () =>
-  orderTotal(emptyFunction,{
+  orderTotal(null, null, {
     items: [
       { name: 'Dragon food', price: 8, quantity: 1 },
       { name: 'Dragon cage (small)',  price: 800, quantity: 1 }
@@ -43,18 +52,12 @@ it('Happy path (Example 1)', () =>
   }).then(result => expect(result).toBe(808)))
 
 it('Happy path (Example 2)', () =>
-  orderTotal(emptyFunction, {
+  orderTotal(null, null, {
     items: [
       { name: 'Dragon collar', price: 20, quantity: 1 },
       { name: 'Dragon chew toy',  price: 40, quantity: 1 }
     ]
   }).then(result => expect(result).toBe(60)))
 
-// Function under test
-function orderTotal(fetch, order) {
-  fetch('https://vatapi.com/v1/country-code-check?code=' + order.country)
-  return Promise.resolve(order.items.reduce((prev, cur) =>
-    cur.price * (cur.quantity || 1) + prev, 0))
-}
-
+module.exports = orderTotal
 // module.exports = orderTotal
