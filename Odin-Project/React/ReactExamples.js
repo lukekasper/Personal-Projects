9a24ecb1d7dcf633fe04a963f9abd88118c2e70b
lukekasper <lukekasper25@gmail.com>
2024-04-11 15:17:18 -0400

    Create ReactExamples.js

---
+++ Odin-Project/React/ReactExamples.js
@@ -0,0 +1,26 @@
+// Loop over a list of items using map function
+function List(props) {
+  return (
+    <ul>
+      // generates a list of li elements for each animal
+      {props.animals.map((animal) => {
+        // conditionally render animal elements starting with an "L"
+        return animal.startsWith("L") ? <li key={animal}>{animal}</li> : null;
+        // optionally use below for the same functionality.  && only works this way in react with boolean value on the left side.
+        // return animal.startsWith("L") && <li key={animal}>{animal}</li>;
+      })}
+    </ul>
+  );
+}
+
+function App() {
+  const animals = ["Lion", "Cow", "Snake", "Lizard"];
+
+  return (
+    <div>
+      <h1>Animals: </h1>
+      // this is where we assign the props as the animals list
+      <List animals={animals} />
+    </div>
+  );
+}
