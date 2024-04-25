a4d27d23b157c1349b4c81d1a88f92716521d32f
lukekasper <lukekasper25@gmail.com>
2024-04-25 11:43:44 -0400

    Update State.js

--- Odin-Project/React/State.js
+++ Odin-Project/React/State.js
@@ -74,3 +74,39 @@ export default function Gallery() {
     </>
   );
 }
+
+
+// Updating states together as a single object
+import { useState } from 'react';
+
+export default function MovingDot() {
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0
+  });
+  return (
+    <div
+      onPointerMove={e => {
+        setPosition({
+          x: e.clientX,
+          y: e.clientY
+        });
+      }}
+      style={{
+        position: 'relative',
+        width: '100vw',
+        height: '100vh',
+      }}>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'red',
+        borderRadius: '50%',
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        left: -10,
+        top: -10,
+        width: 20,
+        height: 20,
+      }} />
+    </div>
+  )
+}
