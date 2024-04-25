525bb74e9f032d6b766d97f9ea97ed1a9387b05e
lukekasper <lukekasper25@gmail.com>
2024-04-25 09:23:49 -0400

    Update ReactExamples.js

--- Odin-Project/React/ReactExamples.js
+++ Odin-Project/React/ReactExamples.js
@@ -24,3 +24,21 @@ function App() {
     </div>
   );
 }
+
+// data with unique ids as keys
+const todos = [
+  { task: "mow the yard", id: uuid() },
+  { task: "Work on Odin Projects", id: uuid() },
+  { task: "feed the cat", id: uuid() },
+];
+
+function TodoList() {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        // here we are using the already generated id as the key.
+        <li key={todo.id}>{todo.task}</li>
+      ))}
+    </ul>
+  );
+}
