e675d1caa833aa1e2e275f2307e4249449526051
lukekasper <lukekasper25@gmail.com>
2024-06-05 15:57:01 -0400

    Update Section.jsx

--- Odin-Project/React/project1/src/components/Section.jsx
+++ Odin-Project/React/project1/src/components/Section.jsx
@@ -6,6 +6,17 @@ function Section({
   line_lst
 }) {
 
+  const [hidden, setHidden] = useState(false);
+
+  function toggleInputVis() {
+    setHidden(!hidden);
+  }
+
+  function submitInfo() {
+    setHidden(!hidden);
+    // TODO - Submit info to update info state variable
+  }
+
   return (
     <div className="section">
       <h2>{title}</h2>
@@ -13,8 +24,15 @@ function Section({
         <Line
           label={line.label}
           info={line.info}
+          hidden={hidden}
         />
       })}
+      <button onClick={toggleInputVis} style={{ display: {hidden} ? 'none' : 'block' }}>
+        Edit {title}
+      </button>
+      <button onClick={submitInfo} style={{ display: {hidden} ? 'block' : 'none' }}>
+        Submit {title}
+      </button>
     </div>
   )
 }
