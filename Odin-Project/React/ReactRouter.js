277cd00073631df673cdd0310bd57958a241d789
lukekasper <lukekasper25@gmail.com>
2024-07-09 14:18:42 -0400

    Create ReactRouter.js

---
+++ Odin-Project/React/ReactRouter.js
@@ -0,0 +1,117 @@
+///////////////// Main /////////////////////
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Profile from "./Profile";
+import DefaultProfile from "./DefaultProfile";
+import Spinach from "./Spinach";
+import Popeye from "./Popeye";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "profile/:name",
+    element: <Profile />,
+    children: [
+      { index: true, element: <DefaultProfile /> },
+      { path: "spinach", element: <Spinach /> },
+      { path: "popeye", element: <Popeye /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+///////////////// Component /////////////////////
+import { Link } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="profile">Profile page</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
+
+///////////////// Component /////////////////////
+import { useParams } from "react-router-dom";
+import DefaultProfile from "./DefaultProfile";
+import Spinach from "./Spinach";
+import Popeye from "./Popeye";
+
+const Profile = () => {
+  const { name } = useParams();
+
+  return (
+    <div>
+      <h1>Hello from profile page!</h1>
+      <p>So, how are you?</p>
+      <hr />
+      <h2>The profile visited is here:</h2>
+      {name === "popeye" ? (
+        <Popeye />
+      ) : name === "spinach" ? (
+        <Spinach />
+      ) : (
+        <DefaultProfile />
+      )}
+    </div>
+  );
+};
+
+export default Profile;
+
+///////////////// Component /////////////////////
+import { Link } from "react-router-dom";
+
+const Popeye = () => {
+  return (
+    <>
+      <p>Hi, I am Popeye! I love to eat Spinach!</p>
+      <Link to="/">Click here to go back</Link>
+    </>
+  );
+};
+
+export default Popeye;
+
+///////////////// Component /////////////////////
+import { Link } from "react-router-dom";
+
+const Spinach = () => {
+  return (
+    <>
+      <p>Hi, I am Spinach! Popeye loves to eat me!</p>
+      <Link to="/">Click here to go back</Link>
+    </>
+  );
+};
+
+export default Spinach;
+
+///////////////// Component /////////////////////
+const DefaultProfile = () => {
+  return <p>Oh, nothing to see here!</p>;
+};
+
+export default DefaultProfile;
+
+///////////////// Component /////////////////////
