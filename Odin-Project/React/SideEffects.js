d11824344a2f81e2f0c070f3c83b9e738c240327
lukekasper <lukekasper25@gmail.com>
2024-07-24 14:37:27 -0400

    Update SideEffects.js

--- Odin-Project/React/SideEffects.js
+++ Odin-Project/React/SideEffects.js
@@ -222,3 +222,84 @@ const Image = () => {
     </>
   );
 };
+
+
+///////////// Fetch Data from an API - Simultaneous Requests /////////////
+
+const useAllData = () => {
+  const [sidebar, setSidebar] = useState();
+  const [comments, setComments] = useState();
+  const [issue, setIssue] = useState();
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      // waiting for allthethings in parallel
+      const result = (
+        await Promise.all([
+          fetch(sidebarUrl),
+          fetch(issueUrl),
+          fetch(commentsUrl),
+        ])
+      ).map((r) => r.json());
+
+      // and waiting a bit more - fetch API is cumbersome
+      const [sidebarResult, issueResult, commentsResult] =
+        await Promise.all(result);
+
+      // when the data is ready, save it to state
+      setSidebar(sidebarResult);
+      setIssue(issueResult);
+      setComments(commentsResult);
+    };
+
+    dataFetch();
+  }, []);
+
+  return { sidebar, comments, issue };
+};
+
+const App = () => {
+  // all the fetches were triggered in parallel
+  const { sidebar, comments, issue } = useAllData();
+
+  // show loading state while waiting for all the data
+  if (!sidebar || !comments || !issue) return 'loading';
+
+  // render the actual app here and pass data from state to children
+  return (
+    <>
+      <Sidebar data={sidebar} />
+      <Issue comments={comments} issue={issue} />
+    </>
+  );
+};
+
+///////////// Fetch Data from an API - Parallel Independent Requests /////////////
+
+fetch('/get-sidebar')
+  .then((data) => data.json())
+  .then((data) => setSidebar(data));
+fetch('/get-issue')
+  .then((data) => data.json())
+  .then((data) => setIssue(data));
+fetch('/get-comments')
+  .then((data) => data.json())
+  .then((data) => setComments(data));
+
+const App = () => {
+  const { sidebar, issue, comments } = useAllData();
+
+  // show loading state while waiting for sidebar
+  if (!sidebar) return 'loading';
+
+  // render sidebar as soon as its data is available
+  // but show loading state instead of issue and comments while we're waiting for them
+  return (
+    <>
+      <Sidebar data={sidebar} />
+      <!-- render local loading state for issue here if its data not available -->
+      <!-- inside Issue component we'd have to render 'loading' for empty comments as well -->
+      {issue ? <Issue comments={comments} issue={issue} /> : 'loading''}
+    </>
+  )
+}
