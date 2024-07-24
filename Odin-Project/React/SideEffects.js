d700cbc715d74f37b2a9de88f3ffe520879b5e64
lukekasper <lukekasper25@gmail.com>
2024-07-24 13:47:39 -0400

    Update SideEffects.js

--- Odin-Project/React/SideEffects.js
+++ Odin-Project/React/SideEffects.js
@@ -190,13 +190,20 @@ import { useEffect, useState } from "react";
 
 const Image = () => {
   const [imageURL, setImageURL] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
-      .then((response) => response.json())
-      .then((response) => setImageURL(response[0].url))
-      .catch((error) => console.error(error));
+  fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
+    .then((response) => {
+      if (response.status >= 400) {
+        throw new Error("server error");
+      }
+      return response.json();
+    })
+    .then((response) => setImageURL(response[0].url))
+    .catch((error) => setError(error));
   }, []);
+
 
   return (
     imageURL && (
