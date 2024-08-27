cd641495abb63eec23f01d419e0be0e2e4d99ea9
lukekasper <lukekasper25@gmail.com>
2024-08-27 10:45:10 -0400

    Update ContextAPI.js

--- Odin-Project/React/ContextAPI.js
+++ Odin-Project/React/ContextAPI.js
@@ -1,1 +1,82 @@
+import { useState, createContext } from "react";
+// other imports for Header and ProductDetail
 
+export const ShopContext = createContext({
+  products: [],
+  cartItems: [],
+  addToCart: () => {},
+});
+
+export default function App() {
+  const [cartItems, setCartItems] = useState([
+    /* List of Items in Cart */
+  ]);
+  const products = /* some custom hook that fetches products and returns the fetched products */
+
+  const addToCart = () => {
+    // add to cart logic (this adds to cartItems)
+  };
+
+  return (
+    /* We are going to pass the things that we want to inject to these components using the value prop */
+    /* This value prop will overwrite the default value */
+    <ShopContext.Provider value={{ cartItems, products, addToCart }}>
+      <Header />
+      <ProductDetail />
+    </ShopContext.Provider>
+  );
+}
+
+
+/// Header Component
+import { useContext } from "react";
+// import for ShopContext
+// import for Link
+
+function Links() {
+  const { cartItems } = useContext(ShopContext); // We must pass the ShopContext object itself as an argument
+
+  return (
+    <ul>
+      {/* Other links */}
+      <li>
+        <Link to="Link to the cart">
+          <span>Cart</span>
+          <div className="cart-icon">{cartItems.length}</div>
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+export default function Header() {
+  return (
+    <header>
+      {/* Other header elements */}
+      <nav>
+        <Links />
+      </nav>
+    </header>
+  );
+}
+
+/// Product Detail Component
+import { useContext } from "react";
+// import for ShopContext
+
+export default function ProductDetail() {
+  const { products, addToCart } = useContext(ShopContext);
+  const product = products.find(/* Logic to find the specific product */);
+
+  return (
+    <div>
+      {/* Image of the product */}
+      <div>
+        {/* elements that align with the design */}
+        <button type="button" onClick={() => addToCart(product)}>
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+}
