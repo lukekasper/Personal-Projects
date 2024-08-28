0107a40d44ead8f0bc48131e89ed93dfd59e44a2
lukekasper <lukekasper25@gmail.com>
2024-08-27 20:22:45 -0400

    Delete CS50/Recipes directory
    Moved to seperate repo

--- CS50/Recipes/cookbook/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import User, Recipe, Comment
-
-# Register your models here.
-admin.site.register(User),
-admin.site.register(Recipe),
-admin.site.register(Comment)
-
