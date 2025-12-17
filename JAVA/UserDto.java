37e3453c2d50d2a010b42208c4921f943d970e21
lukekasper <lukekasper25@gmail.com>
2025-12-17 15:31:46 -0500

    Update UserDto.java

--- JAVA/UserDto.java
+++ JAVA/UserDto.java
@@ -1,7 +1,19 @@
 @AllArgsConstructor
 @Getter
 public class UserDto {
+  /// remove field from json objects
+  @JsonIgnore
   private Long id;
+
+  /// rename field
+  @JsonProperty("username")
   private String name;
   private String email;
+
+  /// Exclude NULL values
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  private String phoneNumber;
+
+  @JsonFormat(pattern = "yyy-MM-dd HH:mm:ss")
+  private LocalDateTime timestamp;
 }
