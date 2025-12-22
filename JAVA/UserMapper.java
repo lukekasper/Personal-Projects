40f4fc29f348c5655dcfbfc87554f17b23e729fb
lukekasper <lukekasper25@gmail.com>
2025-12-22 08:12:59 -0500

    Update UserMapper.java

--- JAVA/UserMapper.java
+++ JAVA/UserMapper.java
@@ -3,3 +3,9 @@ public interface UserMapper {
   @Mapping(target = "timestamp", expression = "java(java.time.LocalDateTime.now())")
   UserDto toDto(User user);
 }
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+  @Mapping(target = "categoryId", source = "category.id")
+  ProductDto toDto(Product product);
+}
