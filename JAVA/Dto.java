f7a08376e8fe21060e920d821acd4f88b2690747
lukekasper <lukekasper25@gmail.com>
2025-12-22 08:14:48 -0500

    Add ProductDto class with product details

--- JAVA/Dto.java
+++ JAVA/Dto.java
@@ -17,3 +17,14 @@ public class UserDto {
   @JsonFormat(pattern = "yyy-MM-dd HH:mm:ss")
   private LocalDateTime timestamp;
 }
+
+
+@AllArgsConstructor
+@Data
+public class ProductDto {
+  private Long id;
+  private String name;
+  private BigDecimal price;
+  private String description;
+  private Byte categoryId;
+}
