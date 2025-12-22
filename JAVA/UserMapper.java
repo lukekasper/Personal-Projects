933d0f298de47210f3de4181ca0e99ecd1d73c2f
lukekasper <lukekasper25@gmail.com>
2025-12-22 08:07:57 -0500

    Fix syntax error in UserMapper annotation

--- JAVA/UserMapper.java
+++ JAVA/UserMapper.java
@@ -1,4 +1,4 @@
-@Mapper(componentModel = "spring)
+@Mapper(componentModel = "spring")
 public interface UserMapper {
   @Mapping(target = "timestamp", expression = "java(java.time.LocalDateTime.now())")
   UserDto toDto(User user);
