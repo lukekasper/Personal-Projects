eac0edcaa1a5796ddb05e187e6e535bcf0cb7ca4
lukekasper <lukekasper25@gmail.com>
2025-12-22 13:13:36 -0500

    Update Student.java

--- JAVA/Kafka/Student.java
+++ JAVA/Kafka/Student.java
@@ -25,6 +25,38 @@ public class Student {
         this.lastName = lastName;
     }
 
+    // Implementing the getters
+    // and setters
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
     @Override
     public String toString()
     {
