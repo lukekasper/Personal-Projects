80667617d77786bdc7cf478a4b59f5423db1be97
lukekasper <lukekasper25@gmail.com>
2025-12-22 13:33:13 -0500

    Update AsyncConfig.java

--- JAVA/Multi-Threading/AsyncConfig.java
+++ JAVA/Multi-Threading/AsyncConfig.java
@@ -1,7 +1,21 @@
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("MyExecutor-");
+        executor.initialize();
+        return executor;
+    }
 }
